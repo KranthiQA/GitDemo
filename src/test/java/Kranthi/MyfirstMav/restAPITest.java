114469d6ac206c493918e906360180e93f68331f
Kranthi <chennam.leelakranthi@gmail.com>
2020-08-06 11:56:57 +0530

    first commit

---
+++ src/test/java/Kranthi/MyfirstMav/restAPITest.java
@@ -0,0 +1,15 @@
+package Kranthi.MyfirstMav;
+
+import org.testng.annotations.Test;
+
+public class restAPITest {
+	@Test
+	public void enterDefectInJira() {
+		System.out.println("enterDefectInJira");
+	}
+	@Test
+	public void deleteTweet() {
+		System.out.println("deleteTweet");
+	}
+
+}
---
+++ src/test/java/Kranthi/MyfirstMav/seleniumTest.java
@@ -0,0 +1,32 @@
+package Kranthi.MyfirstMav;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
+public class seleniumTest {
+	@Test(groups="smoke")
+	public void BrowserAutomation() {
+		DesiredCapabilities ch=DesiredCapabilities.chrome();
+		ch.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
+		ch.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+		ChromeOptions c=new ChromeOptions();
+		c.merge(ch);
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\USER\\Desktop\\chromedriver.exe" );
+		WebDriver d=new ChromeDriver(c);
+		d.get("https://www.google.com/");
+		System.out.println("BrowserAutomation");
+	}
+	@Test(groups= {"smoke"})
+	public void hitURL() {
+		System.out.println("hitURL");
+	}
+	@Test()
+	public void hitsecondURL() {
+		System.out.println("hitsecondURL");
+	}
+
+}
